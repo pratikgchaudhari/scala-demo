316a4d2815e6d1f908a8b912b261e796c1946677
Pratik Chaudhari <chaudharipratikg@gmail.com>
2022-04-02 17:17:43 +0530

    Modified the hello.scala program to accept the user's name from the command line and use it to create a customized greeting message for the user

--- hello-scala/hello.scala
+++ hello-scala/hello.scala
@@ -1,3 +1,10 @@
-object HelloScala extends App {
-println("Hello, Scala!")
+object HelloScala {
+    def hello(name:String):String = 
+        s"Hello, $name"
+    
+    def main(args: Array[String]): Unit =
+        if (args.length != 1)
+            println("Usage: scala hello.scala \"Your First Name\"")
+        else 
+            println(hello(args(0)))
 }
